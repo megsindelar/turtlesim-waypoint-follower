296c9add35c7afadbb740e653b671615c9a8c06f
Meg <megansindelar2023@u.northwestern.edu>
2022-09-29 14:16:59 -0500

    added comments to code

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -9,36 +9,45 @@ from rclpy.node import Node
 class State(Enum):
     """States of program"""
     MOVING = auto(),
-    STOPPING = auto()
+    STOPPED = auto()
 
 class Waypoint(Node):
     def __init__(self):
         super().__init__("waypoint")
-        """Timer that runs at 100 Hz"""
-        self.state = State.STOPPING
+        """Initialize state to be stopping"""
+        self.state = State.STOPPED
         self.stopped = 1
+        """Declare a frequency parameter for timer callback"""
         self.declare_parameter("frequency", 0.01)
         self.frequency = self.get_parameter("frequency").get_parameter_value().double_value
+        """Create a service to toggle states"""
         self.toggle = self.create_service(Empty, "toggle", self.toggle_callback)
+        """Timer that runs at 100 Hz"""
         self.timer = self.create_timer(self.frequency, self.timer_callback)
-        #print(f'State: {self.state}')
     
+    """Timer function that runs at 100 Hz"""
     def timer_callback(self):    
         if self.state == State.MOVING:
+            """Send to debug logger that you are issuing a command as long as it is moving"""
             self.get_logger().debug("Issuing Command!")
             self.stopped = 0
-        elif (self.state == State.STOPPING) and self.stopped == 0:
+        elif (self.state == State.STOPPED) and self.stopped == 0:
+            """When stopping, tell the debug logger once"""
             self.get_logger().debug("Stopping")
             self.stopped = 1
         else:
+            """If already stopped and told debug logger, then print nothing"""
             self.get_logger().debug("")
         print(f'State: {self.state}')
 
+    """Service callback function for toggling states"""
     def toggle_callback(self, request, response):
-        if self.state == State.STOPPING:
+        if self.state == State.STOPPED:
+            """If toggle is called, switch state from stopped to moving"""
             self.state = State.MOVING
         else:
-            self.state = State.STOPPING
+            """If toggle is called, switch state from moving to stopped"""
+            self.state = State.STOPPED
         return response
 
 def main(args=None):
