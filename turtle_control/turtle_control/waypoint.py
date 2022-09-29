be4c523b2ce52277a48a8a19324814b68fd8f816
Meg <megansindelar2023@u.northwestern.edu>
2022-09-29 14:06:30 -0500

    finished the toggle service part of the homework!

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -1,59 +1,50 @@
 import re
 from urllib import request
+from enum import Enum, auto
 import std_srvs
 from std_srvs.srv import Empty
 import rclpy
 from rclpy.node import Node
 
-#class State(Enum):
+class State(Enum):
+    """States of program"""
+    MOVING = auto(),
+    STOPPING = auto()
 
 class Waypoint(Node):
     def __init__(self):
         super().__init__("waypoint")
         """Timer that runs at 100 Hz"""
+        self.state = State.STOPPING
+        self.stopped = 1
         self.declare_parameter("frequency", 0.01)
         self.frequency = self.get_parameter("frequency").get_parameter_value().double_value
-       # self.toggle = self.create_service(Empty, "toggle", self.toggle_callback)
+        self.toggle = self.create_service(Empty, "toggle", self.toggle_callback)
         self.timer = self.create_timer(self.frequency, self.timer_callback)
+        #print(f'State: {self.state}')
     
     def timer_callback(self):    
-        self.get_logger().debug("Issuing Command!")
+        if self.state == State.MOVING:
+            self.get_logger().debug("Issuing Command!")
+            self.stopped = 0
+        elif (self.state == State.STOPPING) and self.stopped == 0:
+            self.get_logger().debug("Stopping")
+            self.stopped = 1
+        else:
+            self.get_logger().debug("")
+        print(f'State: {self.state}')
 
-
-        #def toggle_callback(self, request, response):
-            # if request.toggle == 0:
-            #      self.get_logger().debug("Stopping.")
-                    #!!!!!!!!!!!once!!!!!!!!!! command
-                #stopped = 1   OR is there a way to check last debug log (start with prev log)
-                #if stopped == 1, don't print anything
-            # else:
-            #      self.get_logger().debug("Issuing Command!")
-                #stopped = 0
-
-    # def toggle_callback(self, request, response):
-    #     def check_new_request(self):
-    #         if request.toggle != response.toggle_past:
-    #             new_request = 1
-    #         else:
-    #             new_request = 0
-    #         return new_request
-
-    #     if request.toggle == 0:
-    #         self.get_logger().debug("Stopping.")
-    #     while request.toggle == 1 and not new_request:
-    #         self.get_logger().debug("Issuing Command!")
-    #         new_request = check_new_request()
-
-    #     response.toggle_past = request.toggle
-    #     return response
+    def toggle_callback(self, request, response):
+        if self.state == State.STOPPING:
+            self.state = State.MOVING
+        else:
+            self.state = State.STOPPING
+        return response
 
 def main(args=None):
     """ The main() function. """
     rclpy.init(args=args)
     waypoint = Waypoint()
-    #waypoint.stop = 1
-    #waypoint.new_request = 0
-    #waypoint.toggle_past = 0
     rclpy.spin(waypoint)
     rclpy.shutdown()
 
