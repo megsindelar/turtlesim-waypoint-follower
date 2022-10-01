aa695d5d0a8e9acc554717661d408dd1e11e7ea5
Meg <megansindelar2023@u.northwestern.edu>
2022-10-01 10:21:00 -0500

    edited waypoint file

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -5,11 +5,18 @@ import std_srvs
 from std_srvs.srv import Empty
 import rclpy
 from rclpy.node import Node
+from turtlesim.srv import Spawn, Kill, TeleportAbsolute
+from turtle_interfaces.srv import Waypoints
 
 class State(Enum):
     """States of program"""
     MOVING = auto(),
     STOPPED = auto()
+
+class Turtle(Enum):
+    """States for turtlesim reset and turtle teleporting"""
+    RESET = auto(),
+    TELEPORT = auto()
 
 class Waypoint(Node):
     def __init__(self):
@@ -17,13 +24,34 @@ class Waypoint(Node):
         """Initialize state to be stopping"""
         self.state = State.STOPPED
         self.stopped = 1
+        self.turtle = Turtle.SPAWNING
         """Declare a frequency parameter for timer callback"""
         self.declare_parameter("frequency", 0.01)
         self.frequency = self.get_parameter("frequency").get_parameter_value().double_value
         """Create a service to toggle states"""
         self.toggle = self.create_service(Empty, "toggle", self.toggle_callback)
+        """Create a service for turtle moving to different waypoints"""
+        self.load = self.create_service(Waypoints, "load", self.load_callback)
         """Timer that runs at 100 Hz"""
         self.timer = self.create_timer(self.frequency, self.timer_callback)
+        """Create a client to spawn turtle in new location"""
+        self.spawn = self.create_client(Spawn, "spawn")
+        """Create a client to kill a turtle"""
+        self.kill = self.create_client(Kill, "kill")
+        """Client for resetting the turtle in turtlesim"""
+        self.reset = self.create_client(Empty, "reset")
+        """Client for teleporting turtle in turtlesim"""
+        self.teleport = self.create_client(TeleportAbsolute, "teleport")
+
+        if not self.kill.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "kill" service to become available')
+
+        if not self.spawn.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "spawn" service to become available')
+
+        if not self.reset.wait_for_service(timeout_sec=1.0):
+            raise RuntimeError('Timeout waiting for "spawn" service to become available')
+
     
     """Timer function that runs at 100 Hz"""
     def timer_callback(self):    
@@ -48,6 +76,39 @@ class Waypoint(Node):
         else:
             """If toggle is called, switch state from moving to stopped"""
             self.state = State.STOPPED
+        return response
+
+    def reset_turtlesim(self):
+        self.reset_future = self.reset.call_async()
+
+    def teleport(self):
+        self.teleport_future = 
+
+    def load_callback(self, request, response):
+        """ Callback function for the /load service
+        
+            Resets the turtlesim node to its initial state and creates waypoints at those locations,
+            then the turtle moves to each waypoint and calculates the total linear distance traveled
+        """
+
+        if self.state == State.STOPPED:
+            self.reset_turtlesim()
+            self.state = State.MOVING
+        else:
+            
+
+
+        # self.get_logger().info("Killing turtle1")
+        # self.kill_future = self.kill.call_async(Kill.Request(name="turtle1"))
+        # self.turtle = Turtle.KILLING
+
+        # #Calculate a new random position for the respawned turtle
+        # self.x_new = request.mixer.y + request.mixer.angular_velocity
+        # self.y_new = request.mixer.x * request.mixer.linear_velocity
+
+        # response.x = self.x_new
+        # response.y = self.y_new
+
         return response
 
 def main(args=None):
